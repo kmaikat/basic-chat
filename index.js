c401de6206139450dc6bd48fa8d0c55559fd56e3
Katherine <katherinepmai@gmail.com>
2023-08-07 11:28:10 -0500

    can now emmit chat message event

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require('express');
 const app = express(); // express initializes app to be a function handler that gets supplied to an HTTP server (line 4)
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 // route handler for the website homepage
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world!!!!!!!!!!! 😀 </h1>');
+    res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+});
+
 
 // server is listening on port 3000
 server.listen(3000, () => {
